c8617ecafe7379c111d4c820ac1c0f017bc13280
jlcampbell16 <45213854+Jlcampbell16@users.noreply.github.com>
2019-04-29 09:40:20 -0600

    Updates

---
+++ precache-manifest.736fc813d5fedfd0d44f732d9b03e8d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "586e355d320f9bac7686a1d2b1f6adfa",
+    "url": "/Clicky-Game2/index.html"
+  },
+  {
+    "revision": "717d52fbf0a160943baf",
+    "url": "/Clicky-Game2/static/css/main.1e4a3091.chunk.css"
+  },
+  {
+    "revision": "5b8bbdbb75b8b44c442b",
+    "url": "/Clicky-Game2/static/js/2.6fcf23cc.chunk.js"
+  },
+  {
+    "revision": "717d52fbf0a160943baf",
+    "url": "/Clicky-Game2/static/js/main.19c37115.chunk.js"
+  },
+  {
+    "revision": "c599b798ed1755800567",
+    "url": "/Clicky-Game2/static/js/runtime~main.83b6945d.js"
+  }
+]);
